ec0652ceb237d83bf3df47a118d831a678765880
dattranuit <18520590@gm.uit.edu.vn>
2020-11-28 23:10:51 +0700

    Add bottom navigation view and some fragment

--- app/src/main/java/com/dattranuit/uitapp/MainActivity.java
+++ app/src/main/java/com/dattranuit/uitapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.dattranuit.uitapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment fragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_tkb:
+                            fragment = new TkbFragment();
+                            getSupportActionBar().setTitle("Thoi Khoa Bieu");
+                            break;
+                        case R.id.nav_lichthi:
+                            fragment = new LichthiFragment();
+                            getSupportActionBar().setTitle("Lich Thi");
+                            break;
+                        case  R.id.nav_canhan:
+                            fragment = new CanhanFragment();
+                            getSupportActionBar().setTitle("Ca Nhan");
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            fragment).commit();
+                    return true;
+                }
+            };
 }
